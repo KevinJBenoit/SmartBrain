3b8b024fae9ed3f7080743b7e2211d977f129ca8
Kevin Benoit <kevin.benoit8@gmail.com>
2019-09-04 13:35:24 -0600

    added particles

--- src/App.js
+++ src/App.js
@@ -4,18 +4,34 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
 
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/* <FaceRecoginition /> */}
-    </div>
-  );
+const particleOptions = {
+  polygon: {
+    enable: true,
+    type: 'inside',
+    move: {
+        radius: 10
+    },
+    url: 'path/to/svg.svg'
+  }
+}
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Particles className='particles' params={{particleOptions}} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm />
+        {/* <FaceRecoginition /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
